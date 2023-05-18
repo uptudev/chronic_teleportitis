e2ffaabf2a3eeb192b79ce31406c3c95e56c5fcf
uptu <uptu@uptu.dev>
2023-05-18 16:14:46 -0600

    Teleportigen v0.0.2 (Workshop Release)

--- init.lua
+++ init.lua
@@ -1,15 +1,12 @@
+dofile_once("data/scripts/perks/perk.lua")
+
 function OnPlayerSpawned( player_entity ) -- This runs when player entity has been created
-	GamePrint("GOOD LUCK")
+	GamePrint("Extra-Hard Mode Enabled")
     local x, y = EntityGetTransform(player_entity)
     local tele = perk_spawn(x, y, "TELEPORTITIS")
     local tele_dodge = perk_spawn(x, y, "TELEPORTITIS_DODGE")
-    perk_pickup(perk, player_entity, EntityGetName(tele), false, false, false)
-    perk_pickup(perk, player_entity, EntityGetName(tele_dodge), false, false, false)
-end
-
-function OnMagicNumbersAndWorldSeedInitialized() -- this is the last point where the Mod* API is available. after this materials.xml will be loaded.
-	local x = ProceduralRandom(0,0)
-	print( "===================================== random " .. tostring(x) )
+    perk_pickup(tele, player_entity, EntityGetName(tele), false, false, false)
+    perk_pickup(tele_dodge, player_entity, EntityGetName(tele_dodge), false, false, false)
 end
 
 print("Teleportigen init done")
